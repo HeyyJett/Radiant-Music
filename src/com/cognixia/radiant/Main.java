776dc1d21f4edff6d6c649e60d62814ce614896d
Smithy <jsmit30@unh.newhaven.edu>
2023-10-31 13:29:06 -0400

    startrd menu

--- src/com/cognixia/radiant/Main.java
+++ src/com/cognixia/radiant/Main.java
@@ -1,8 +1,25 @@
 package com.cognixia.radiant;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("Welcome to The Radiant Music Network!");
+        System.out.println("-------------------------------------");
+        
+        System.out.println("1: Explore song list");
+        System.out.println("2: Login");
+        int userOption = sc.nextInt();
+        
+        
+        
+        
+        sc.close();
+        
+        
     }
 }
