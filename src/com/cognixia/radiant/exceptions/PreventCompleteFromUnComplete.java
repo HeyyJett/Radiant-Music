6e8b7f4a4e059a772d757485ec6fdd33bdd7da77
Cris <crisherrera024@gmail.com>
2023-10-31 16:23:25 -0500

    Implemented PreventCompleteFromUncomplete Exception

--- src/com/cognixia/radiant/exceptions/PreventCompleteFromUnComplete.java
+++ src/com/cognixia/radiant/exceptions/PreventCompleteFromUnComplete.java
@@ -1,6 +1,9 @@
 package com.cognixia.radiant.exceptions;
 
-public class PreventCompleteFromUnComplete {
+public class PreventCompleteFromUnComplete extends Exception {
 
-    
+    public PreventCompleteFromUnComplete(){
+        super("You cannot move this music from 'Uncompleted' to 'Completed'. You must move music to 'In-Progress' first.");
+    }
+
 }
